066cb3ab26a831e58a7fb1a1f006f5150ecd59f2
A7med3bdulBaset <A7med3bdulBaset@gmail.com>
2023-05-06 00:06:43 +0300

    Create File Function

---
+++ src/helpers/createFile.ts
@@ -0,0 +1,30 @@
+import { existsSync } from "fs";
+import {writeFile } from "fs/promises";
+import nodePath from "path";
+
+/**
+ * Create File (e.g. db/bukhari/1.json)
+ * 
+ * @param path Path to file (e.g. ["bukhari"])
+ * @param fileName Name of file (e.g. "1")
+ * @param data Data to be written to file
+ */
+async function createFile(mainDir: string[] = ["db"], path: string[], fileName: string, data: any) {
+	const resolvedPath = nodePath.join(
+		process.cwd(),
+		...mainDir,
+		...path,
+		fileName + ".json"
+	);
+	const textContent = JSON.stringify(data);
+	// const textContent = JSON.stringify(data, null, 2); //? For pretty JSON
+	
+	// Exit if file already exists
+	// if (existsSync(resolvedPath)) return
+
+	// Write content to file
+	await writeFile(resolvedPath, textContent);
+	console.info(`File "${path.join("/")}/${fileName}.json" created!`)
+}
+
+export default createFile;
